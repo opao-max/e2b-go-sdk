543ced9c5d88d3f38a2b30a60d7480397e9bedd0
opao-max <2709255936@qq.com>
2026-07-15 09:40:00 +0800

    feat: images and export aliases

---
+++ export_aliases.go
@@ -0,0 +1,79 @@
+package e2b
+
+import (
+	rootcmd "github.com/superduck-ai/e2b-go-sdk/commands"
+	rootfs "github.com/superduck-ai/e2b-go-sdk/filesystem"
+	rootgit "github.com/superduck-ai/e2b-go-sdk/git"
+)
+
+const (
+	FileTypeFile = rootfs.FileTypeFile
+	FileTypeDir  = rootfs.FileTypeDir
+)
+
+const (
+	ReadFormatText   = rootfs.ReadFormatText
+	ReadFormatBytes  = rootfs.ReadFormatBytes
+	ReadFormatStream = rootfs.ReadFormatStream
+	ReadFormatBlob   = rootfs.ReadFormatBlob
+)
+
+const (
+	FilesystemEventChmod  = rootfs.FilesystemEventChmod
+	FilesystemEventCreate = rootfs.FilesystemEventCreate
+	FilesystemEventRemove = rootfs.FilesystemEventRemove
+	FilesystemEventRename = rootfs.FilesystemEventRename
+	FilesystemEventWrite  = rootfs.FilesystemEventWrite
+)
+
+type FileType = rootfs.FileType
+type Blob = rootfs.Blob
+type WriteInfo = rootfs.WriteInfo
+type EntryInfo = rootfs.EntryInfo
+type Filesystem = rootfs.Filesystem
+type ReadFormat = rootfs.ReadFormat
+type FilesystemRequestOpts = rootfs.FilesystemRequestOpts
+type FilesystemWriteOpts = rootfs.FilesystemWriteOpts
+type FilesystemReadOpts = rootfs.FilesystemReadOpts
+type FilesystemListOpts = rootfs.FilesystemListOpts
+type WatchOpts = rootfs.WatchOpts
+type WriteEntry = rootfs.WriteEntry
+type FilesystemEventType = rootfs.FilesystemEventType
+type FilesystemEvent = rootfs.FilesystemEvent
+type WatchHandle = rootfs.WatchHandle
+
+type CommandExitError = rootcmd.CommandExitError
+type CommandResult = rootcmd.CommandResult
+type Stdout = rootcmd.Stdout
+type Stderr = rootcmd.Stderr
+type PtyOutput = rootcmd.PtyOutput
+type CommandHandle = rootcmd.CommandHandle
+type ProcessInfo = rootcmd.ProcessInfo
+type CommandRequestOpts = rootcmd.CommandRequestOpts
+type CommandConnectOpts = rootcmd.CommandConnectOpts
+type CommandStartOpts = rootcmd.CommandStartOpts
+type Commands = rootcmd.Commands
+type Pty = rootcmd.Pty
+type PtySize = rootcmd.PtySize
+type PtyCreateOpts = rootcmd.PtyCreateOpts
+type PtyConnectOpts = rootcmd.PtyConnectOpts
+
+type Git = rootgit.Git
+type GitRequestOpts = rootgit.GitRequestOpts
+type GitCloneOpts = rootgit.GitCloneOpts
+type GitInitOpts = rootgit.GitInitOpts
+type GitRemoteAddOpts = rootgit.GitRemoteAddOpts
+type GitCommitOpts = rootgit.GitCommitOpts
+type GitAddOpts = rootgit.GitAddOpts
+type GitDeleteBranchOpts = rootgit.GitDeleteBranchOpts
+type GitPushOpts = rootgit.GitPushOpts
+type GitPullOpts = rootgit.GitPullOpts
+type GitResetOpts = rootgit.GitResetOpts
+type GitRestoreOpts = rootgit.GitRestoreOpts
+type GitResetMode = rootgit.GitResetMode
+type GitDangerouslyAuthenticateOpts = rootgit.GitDangerouslyAuthenticateOpts
+type GitConfigOpts = rootgit.GitConfigOpts
+type GitConfigScope = rootgit.GitConfigScope
+type GitBranches = rootgit.GitBranches
+type GitFileStatus = rootgit.GitFileStatus
+type GitStatus = rootgit.GitStatus
